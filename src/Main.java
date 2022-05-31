e378c9e7f1a40a29dc02c92da957e8724ef4305e
irekhusainov <i@khusainovirek>
2022-05-31 09:44:33 +0300

    HW-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int I = 0;
+        while (I < 10) {
+            I++;
+            System.out.print(I + " ");
+        }
+        System.out.println();
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        //Задание №2
+        int f = 5;
+        int necessaryFriday = f;
+        for (; necessaryFriday <= 31; necessaryFriday = necessaryFriday + 7) {
+            System.out.println("Сегодня пятница " + necessaryFriday + "-е число. Нужно подготовить отчет");
+        }
+        // Задание №3
+        int year = 2022 - 200;
+        int lastYear = 2022 + 100;
+        while (year <= lastYear) {
+            year++;
+            if (year % 79 == 0)
+                System.out.println(year);
+        }
     }
 }
